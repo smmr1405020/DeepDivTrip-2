74a277f074427a68d8bf2ab243b28c0f21e7ae37
Mukit <mukitrashid270596@gmail.com>
2021-04-16 02:39:44 +0600

    intra f1 added

--- mainfile.py
+++ mainfile.py
@@ -13,4 +13,4 @@ generate_ds(args_kdiverse.dat_ix, args_kdiverse.FOLD, args_kdiverse.test_index)
 
 from kdiverse_generator import generate_result
 
-generate_result(True, K=3, N_min=5, N_max=5)
+generate_result(True, K=3, N_min=9, N_max=9)
