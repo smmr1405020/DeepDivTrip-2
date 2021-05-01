9147cb161076aee68a2bcf7a9f1a0648564be9b7
Mukit <mukitrashid270596@gmail.com>
2021-05-02 05:06:12 +0600

    deepdivtrip variant commit

--- args_kdiverse.py
+++ args_kdiverse.py
@@ -1,4 +1,4 @@
-dat_ix = 2
+dat_ix = 3
 FOLD = 10
 test_index = 7
 copy_no = 0
--- parameters.py
+++ parameters.py
@@ -4,4 +4,4 @@ ae_num_epoch = 20000
 ae_learning_rate = 0.01
 
 lstm_model_batch_size = 32
-lstm_model_hidden_size = 32
+lstm_model_hidden_size = 256
