dfb8e419d347d20168f76fed3116ae8a14c8541b
Mukit <mukitrashid270596@gmail.com>
2021-05-02 16:36:07 +0600

    deepdivtrip variant commit

--- lstm_model.py
+++ lstm_model.py
@@ -20,10 +20,7 @@ class TrajPredictor(nn.Module):
         super(TrajPredictor, self).__init__()
 
         self.embedding = nn.Embedding(len(data_generator.vocab_to_int) - 3,
-                                      embedding_dim=pretrained_node_embeddings.shape[1]).from_pretrained(
-            pretrained_node_embeddings,
-            freeze=False)
-
+                                      embedding_dim=pretrained_node_embeddings.shape[1])
         '''
 
         '''
